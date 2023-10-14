26856bc59c677aaf248865dfa38fd0c47aae8ffd
Héctor Ocampo <hector2603@users.noreply.github.com>
2023-10-14 15:33:54 -0500

    init application

---
+++ calculator-app/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Login() {
+  return (
+    <main className={styles.main}>
+      <h1>Login App</h1>
+    </main>
+  )
+}
